ff2a0e49a9734398e4e2f7563f1329025d960b29
Devin Turvey <dturvey@Devins-MacBook-Pro-2.local>
2023-08-07 16:53:01 -0400

    Welcome Layout

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     "./src/**/*"
 ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
   },
   plugins: [],
 }
